13363ac1102739bb1fe1322bf3bf3108b6492332
Thiago Leonardo <lthiago2019@gmail.com>
2026-03-19 15:13:01 -0300

    Fix DATABASE_URL read

--- app/const/url.py
+++ app/const/url.py
@@ -3,17 +3,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DOCKER_COMPOSE = os.getenv("DOCKER_COMPOSE")
-ENV_DATABASE_URL = os.getenv("ENV_DATABASE_URL")
-POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
-POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
-POSTGRES_HOST = os.getenv("POSTGRES_HOST", "parcerias_ufpa-v1")
-POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
-POSTGRES_DB = os.getenv("POSTGRES_DB", "PARCERIAS_UFPA_DB")
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-if ENV_DATABASE_URL:
-    DATABASE_URL = f"postgresql+asyncpg://postgres:{ENV_DATABASE_URL}"
-elif DOCKER_COMPOSE == "true":
-    DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
-else:
-    DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
+if not DATABASE_URL:
+    DOCKER_COMPOSE = os.getenv("DOCKER_COMPOSE")
+    ENV_DATABASE_URL = os.getenv("ENV_DATABASE_URL")
+    POSTGRES_USER = os.getenv("POSTGRES_USER", "postgres")
+    POSTGRES_PASSWORD = os.getenv("POSTGRES_PASSWORD", "postgres")
+    POSTGRES_HOST = os.getenv("POSTGRES_HOST", "parcerias_ufpa-v1")
+    POSTGRES_PORT = os.getenv("POSTGRES_PORT", "5432")
+    POSTGRES_DB = os.getenv("POSTGRES_DB", "PARCERIAS_UFPA_DB")
+
+    if ENV_DATABASE_URL:
+        DATABASE_URL = f"postgresql+asyncpg://postgres:{ENV_DATABASE_URL}"
+    elif DOCKER_COMPOSE == "true":
+        DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@db:5432/{POSTGRES_DB}"
+    else:
+        DATABASE_URL = f"postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_HOST}:{POSTGRES_PORT}/{POSTGRES_DB}"
